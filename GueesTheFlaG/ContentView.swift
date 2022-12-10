f046ea2b6e14e67f8ea66479e9cfa2d43601dad7
Asanov Sanzhar <asnvsn@mail.ru>
2022-12-10 07:28:26 +0600

    Initial Commit

---
+++ GueesTheFlaG/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  GueesTheFlaG
+//
+//  Created by Санжар Асанов on 10/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var countries = ["uk", "usa", "argentina", "brazil", "germany", "bangladesh", "canada", "greece", "russia", "sweden"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack {
+                    Text("Выбери флаг")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                ForEach(0..<3) {number in
+                    Button(action: {
+                        self.flagTapped(number)
+                        self.showingScore = true
+                    }) {
+                        Image(self.countries[number])
+                            .frame(width: 250, height: 125)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Text("Общий счет: \(score)")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                Spacer()
+            }
+        } .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Общий счет: \(score)"), dismissButton: .default(Text("Продолжить")) {
+                self.askQuestion()
+            })
+        }
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Правильный ответ!"
+            score += 1
+        } else {
+            scoreTitle = "Неправильно! Это флаг \(countries[number])"
+            score -= 1
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GueesTheFlaG/GueesTheFlaGApp.swift
@@ -0,0 +1,17 @@
+//
+//  GueesTheFlaGApp.swift
+//  GueesTheFlaG
+//
+//  Created by Санжар Асанов on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GueesTheFlaGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
